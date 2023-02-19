ef3039cf692b73f7bc6ab2e58a241b853832c568
marek0209 <marek.urbaniec1@gmail.com>
2023-02-19 02:40:53 +0100

    Add a function to calculate value of cards

---
+++ src/services/GameLogicService/index.js
@@ -0,0 +1,21 @@
+const GameLogicService = {
+  getScore: (card, score) => {
+    if (card) {
+      switch (card.value) {
+        case "KING":
+        case "JACK":
+        case "QUEEN":
+          return 10;
+
+        case "ACE":
+          return score + 11 > 21 ? 1 : 11;
+
+        default:
+          return parseInt(card.value);
+      }
+    }
+
+    return score;
+  },
+};
+export default GameLogicService;
